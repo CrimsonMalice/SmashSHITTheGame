be928bd3e746796b4ffa420f4d7e3d9c3116192f
CrimsonMalice <malusnosferatu@gmail.com>
2017-08-26 01:58:57 +0200

    Added some fun shit

---
+++ Assets/Scripts/CatScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatScript : MonoBehaviour {
+
+    [SerializeField] public bool killCat = false;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (killCat)
+        {
+            gameObject.transform.localScale -= new Vector3(0.1f, 0.1f, 0.1f);
+            gameObject.transform.position += new Vector3(15, 15, 0);
+        }
+
+        if (gameObject.transform.localScale.x <= 0f)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     [SerializeField] private float speed = 30;
+    [SerializeField] private float attackDelay;
     [SerializeField] private Vector2 velocity;
     [SerializeField] private bool attacking = false;
+    [SerializeField] private bool canAttack = true;
 
     private Rigidbody2D rbody;
+    [SerializeField] private GameObject playerAttack;
 
 	// Use this for initialization
 	void Awake ()
@@ -20,10 +23,35 @@ public class PlayerController : MonoBehaviour {
 	void FixedUpdate ()
     {
         Move();
+
+        Attack();
 	}
 
     void Move()
     {
         rbody.velocity = new Vector2(speed, 0);
+    }
+
+    void Attack()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && canAttack)
+        {
+            Vector3 attackVector = new Vector3(gameObject.transform.position.x + 55, gameObject.transform.position.y + 15, gameObject.transform.position.z);
+            Instantiate(playerAttack, attackVector, Quaternion.identity);
+
+            canAttack = false;
+            attackDelay = 0.4f;
+        }
+
+        if (attackDelay > 0)
+        {
+            attackDelay -= Time.deltaTime;
+        }
+
+        else if (attackDelay <= 0)
+        {
+            canAttack = true;
+            attackDelay = 0;
+        }
     }
 }
